539af029fbf64856f672b63388c5f26b19aea48f
Gil Ben <Gil Ben>
2017-01-14 11:53:40 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,90 @@
+const 	express 		= require('express'),
+		//template engine 
+		hbs				= require('hbs'),
+		//fs - file system
+		//built in node
+	 	fs 				= require('fs');
+
+
+//starting express
+var app = express();
+
+//----------------
+//hbs 
+//----------------
+//partials folder
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+//serving static files
+
+//==============================
+//Middleware (runs by order)
+//==============================
+
+//Maintenance message - above express.static - commented on/off
+// app.use((req,res, next) => {
+// 	res.render('partials/maintenance.hbs');
+// 	//don't need next()
+// });
+
+//Express serving static files
+app.use(express.static(__dirname + '/public'));
+
+//Log Server requests 
+app.use((req,res,next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+
+	  //logging:
+	  console.log(log);	 
+	  //(file name, what we add)
+	  fs.appendFile('server.log', log + '\n', (err)=> {
+	  	if (err) {
+	  		console.log(`unable to append to server.log.`);
+	  	}
+	  });
+	next();
+});
+
+
+
+//--------------------------------------
+//hbs helpers
+//--------------------------------------
+//functions that dynamically create content that can run from an hbs
+//name of helper, callback function
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+
+//Routes
+//======================================
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
